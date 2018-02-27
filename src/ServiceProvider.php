fc6013112a7522c1fb15ff681b0b5b694269b728
Julien Bourdeau <julien@sigerr.org>
2018-02-27 12:19:15 +0100

    Release v1.0.1

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,7 +12,7 @@ class ServiceProvider extends LaravelServiceProvider
 {
     public function boot()
     {
-        AlgoliaUserAgent::addSuffixUserAgentSegment('; Laravel Scout settings package', '1.0.0');
+        AlgoliaUserAgent::addSuffixUserAgentSegment('; Laravel Scout settings package', '1.0.1');
 
         if (!class_exists('AlgoliaSearch\Client')) {
             throw new Exception("It seems like you are not using Algolia. This package requires the Algolia engine.", 1);
